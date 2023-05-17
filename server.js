49569626ccd5f9efe9cdac124bc4604399279fae
Alec Wixom <atwixom@gmail.com>
2023-05-17 15:28:32 -0600

    Initial upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+
+app.listen(4000, () => {
+    console.log('Were up on 4000!');
+})
