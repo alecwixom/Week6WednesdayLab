cb62569211e83a6d4f29515304d4b874d045135e
Alec Wixom <atwixom@gmail.com>
2023-05-17 15:31:10 -0600

    Changed server port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
 
-app.listen(4000, () => {
-    console.log('Were up on 4000!');
+app.listen(4001, () => {
+    console.log('Were up on 4001!');
 })
